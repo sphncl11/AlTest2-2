d958a7446e8529bc247c9d605b0dc87ddb2c0d99
Jason Apacible <jasonapa23@gmail.com>
2026-02-14 12:38:55 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ACCOUNT MANAGEMENT");
+
+            string username = "usernamemo";
+            string password = "urpassword11";
+
+            Console.Write("Enter username: ");
+            string usernameInput = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string passwordInput = Console.ReadLine();
+
+            if (usernameInput == username && passwordInput == password) {
+                Console.WriteLine("Login Sucessful.");
+            }
+            else {
+                Console.WriteLine("Incorrect. Please try again.");
+            }
+        }
+    }
+}
