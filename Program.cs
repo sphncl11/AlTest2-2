6cbc0044939f3ccda530cc8589a38412f3b7987f
sphncl11 <sophianicolesalmendral11@gmail.com>
2026-02-21 11:46:20 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StoreManagement2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n----- ReySal STORE MANAGEMENT SYSTEM -----");
+            Console.WriteLine("You can select the options below: ");
+            Console.WriteLine("1 - Add Branch");
+            Console.WriteLine("2 - ");
+            Console.WriteLine("3 - ");
+            Console.WriteLine("4 - ");
+            Console.WriteLine("5 - ");
+            Console.Write("Select option (1-5): ");
+            string optionInput = Console.ReadLine();
+
+            /*switch (optionInput)
+            {
+                case 1:
+                    //add branch
+                    break;
+                case 2:
+                    //view branches
+                    break;
+                case 3:
+                    //update branch
+                    break;
+                case 4:
+                    //delete branch
+                    break;
+                case 5:
+                    //exit
+                    break;
+            }*/
+
+
+        }
+    }
+}
